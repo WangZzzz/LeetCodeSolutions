3cdf4511e4702007ab108fb1c48e260fcf9dc4c1
wangzhe15 <>
2020-07-22 11:05:00 +0800

    update

--- src/sort/QuickSort.java
+++ src/sort/QuickSort.java
@@ -3,6 +3,7 @@ package sort;
 import util.CommonUtils;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * 快速排序
@@ -11,7 +12,7 @@ public class QuickSort {
     public static void main(String[] args) {
         int[] array = CommonUtils.createTestArray(20);
         System.out.println(Arrays.toString(array));
-        quickSort(array, 0, array.length - 1);
+        quickSort2(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array));
     }
 
@@ -31,18 +32,49 @@ public class QuickSort {
         int key = array[start];
 
         while (i < j) {
-            while (array[j] >= key & i < j) {
+            while (i < j & array[j] >= key) {
                 j--;
             }
-            array[i] = array[j];
-
-            while (array[i] <= key & i < j) {
+            if (i < j) {
+                //a[j] 挖了个坑 填到 a[i], a[j]多了个坑
+                array[i] = array[j];
                 i++;
             }
-            array[j] = array[i];
 
+            while (i < j & array[i] <= key) {
+                i++;
+            }
+            if (i < j) {
+                //填上a[j]的坑
+                array[j] = array[i];
+                j--;
+            }
         }
         array[i] = key;
         return i;
+    }
+
+    public static void quickSort2(int[] arr, int start, int end) {
+        if (start < end) {
+            int i = start;
+            int j = end;
+            int key = arr[i];
+            while (i < j) {
+                while (i < j & arr[j] > key) {
+                    j--;
+                }
+
+                arr[i] = arr[j];
+
+                while (i < j & arr[i] < key) {
+                    i++;
+                }
+
+                arr[j] = arr[i];
+            }
+            arr[i] = key;
+            quickSort(arr, start, i - 1);
+            quickSort(arr, i + 1, end);
+        }
     }
 }
